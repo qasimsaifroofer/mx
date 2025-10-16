9f72fe0998e5ddc9b52cbc1fe12dcc95b2a513d9
qasimsaifroofer <qasimroofer@gmail.com>
2025-10-16 12:27:57 +0500

    sas
    asas

--- app/blogs/streamline-property-claims-estimating/page.jsx
+++ app/blogs/streamline-property-claims-estimating/page.jsx
@@ -37,7 +37,7 @@ export default function BlogPost() {
               Xactimate, developed by Verisk, remains the industry’s gold standard for property claims estimating software.
               Yet, despite its widespread use, many professionals only scratch the surface of its potential. In this guide,
               we’ll explore how to streamline property claims using Xactimate, improve estimate precision, and leverage
-              professional <a href="https://mxestimation.com/">Xactimate estimating services</a> to boost business efficiency.
+              professional <a class="post" href="https://mxestimation.com/">Xactimate estimating services</a> to boost business efficiency.
             </p>
 
             <h2 className="text-2xl font-semibold mb-4 text-white">
@@ -209,5 +209,6 @@ export default function BlogPost() {
     </>
   );
 }
+
 
 
